70edf9ba3ba453b78c293fbf36a8a26c761e34fa
JokerWon <1595672103@qq.com>
2023-04-18 00:30:51 +0800

    feat: dev on vite

---
+++ playground/test-fc/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return <div>Hello World</div>;
+}
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <App />
+);
